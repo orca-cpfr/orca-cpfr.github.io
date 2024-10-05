76f8fa2e732e3a10b7ec610e2e69278333024048
imantung <iman.tung@gmail.com>
2024-10-06 05:25:54 +0700

    implement google font

--- landing-page/tailwind.config.js
+++ landing-page/tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif'],
+        'maven': ['Maven Pro', 'sans-serif']
+      },
       colors: {
         primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
       }
