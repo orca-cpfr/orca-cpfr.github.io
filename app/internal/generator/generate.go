ee0826ee3f735b84bd5f068916158bef8b0d577e
imantung <iman.tung@gmail.com>
2024-10-07 00:38:12 +0700

    add menu for dashboard

--- app/internal/generator/generate.go
+++ app/internal/generator/generate.go
@@ -11,15 +11,48 @@ var (
 	Root   = "src/pages"
 	OutDir = "public"
 	Data   = struct {
-		PageName string
-		Meta     Meta
-		HomeURL  string
+		PageName   string
+		Meta       Meta
+		HomeURL    string
+		GroupMenus []GroupMenu
 	}{
 		Meta: Meta{
 			Title:       "orca-cpfr.io | AI-Driven CPFR Platform",
 			Description: "AI-Driven Platform for Reliable Strategic Planning, and Operational Mitigation Actions with Zero Learning",
 		},
 		HomeURL: "/index.html",
+		GroupMenus: []GroupMenu{
+			{
+				Name: "PRINCIPAL",
+				Menus: []Menu{
+					{Name: "Master Distributor", URL: "#", Icon: "table"},
+					{Name: "Store Reports", URL: "#", Icon: "pie"},
+					{Name: "Order Forecasting", URL: "#", Icon: "insight"},
+					{Name: "Sales Forecasting", URL: "#", Icon: "insight"},
+					{Name: "Principal Inventory", URL: "#", Icon: "box"},
+				},
+			},
+			{
+				Name: "DISTRIBUTOR",
+				Menus: []Menu{
+					{Name: "Master Salesman", URL: "#", Icon: "table"},
+					{Name: "Master Store", URL: "#", Icon: "table"},
+					{Name: "Salesman Reports", URL: "#", Icon: "pie"},
+					{Name: "Distributor Inventory", URL: "#", Icon: "box"},
+					{Name: "Distributor Replenishment", URL: "#", Icon: "box"},
+					{Name: "Store Replenishment", URL: "#", Icon: "box"},
+				},
+			},
+			{
+				Name: "SALESMAN",
+				Menus: []Menu{
+					{Name: "Find Nearby Store", URL: "#", Icon: "device"},
+					{Name: "Survey Store", URL: "#", Icon: "device"},
+					{Name: "Order Proposal", URL: "#", Icon: "device"},
+					{Name: "Salesman Activity Logs", URL: "#", Icon: "pie"},
+				},
+			},
+		},
 	}
 )
 
@@ -27,6 +60,15 @@ type (
 	Meta struct {
 		Title       string
 		Description string
+	}
+	GroupMenu struct {
+		Name  string
+		Menus []Menu
+	}
+	Menu struct {
+		Name string
+		URL  string
+		Icon string
 	}
 )
 
